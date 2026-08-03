fc9bff420798f1502c95a6fd6c71b7eaadbe9d58
yakumo-hikari <ahmadabduraimov7@gmail.com>
2026-08-03 15:41:55 +0500

    engine.py

---
+++ engine.py
@@ -0,0 +1,9 @@
+class AAMEngine:
+
+    def calculate(self, tsh, t4, atpo):
+
+        return {
+            "status": "Demo mode",
+            "message":
+            "Core analytical algorithms are not included in the public repository."
+        }
